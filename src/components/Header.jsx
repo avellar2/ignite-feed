d68ca73a9e6e1eee988d792390c8673fc487fb0e
Vanderson <vandimavellar1997@gmail.com>
2024-12-26 16:00:12 -0300

    component Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import styled from './Header.module.css'
+import igniteLogo from '../assets/ignite-logo.svg'
 export function Header() {
     return (
         <header className={styled.header}>
-            <strong>Ignite Feed</strong>
+            <img src={igniteLogo} alt='logo do ignite'/>
         </header>
     )
 }
